ce6a109b668bdd6ed8f1ab01789f0af3214c277c
kiyon <kiyonlin@163.com>
2019-05-17 17:28:21 +0800

    初始化抓取pdf程序

--- fetcher/main.ts
+++ fetcher/main.ts
@@ -6,7 +6,7 @@ import * as puppeteer from 'puppeteer';
 
     await page.goto("https://blog.angularindepth.com/angulars-digest-is-reborn-in-the-newer-version-of-angular-718a961ebd3e");
 
-    await page.pdf({ path: '../pdf/angulars-digest-is-reborn-in-the-newer-version-of-angular.pdf', format: 'A4' });
+    await page.pdf({ path: './pdf/angulars-digest-is-reborn-in-the-newer-version-of-angular.pdf', format: 'A4' });
 
     await browser.close();
 })();
